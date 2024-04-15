c3a12a09337494c391c8d2021140a6ec379bb480
maitzeth <andre801mz@gmail.com>
2024-04-15 01:42:57 -0300

    feat: adding styling to tab navigation

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -6,13 +6,50 @@ import {Muestras} from '../screens/Muestras';
 import {Details} from '../screens/Details';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Icon from 'react-native-vector-icons/Octicons';
+import {theme} from '../utils/theme';
+import Color from 'color';
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
+const screenOptions = (route: string, color: string) => {
+  let iconName;
+
+  switch (route) {
+    case 'Home':
+      iconName = 'home';
+      break;
+    case 'Muestras':
+      iconName = 'repo';
+      break;
+    case 'Vademecum':
+      iconName = 'book';
+      break;
+    default:
+      break;
+  }
+
+  return <Icon name={iconName} color={color} size={24} />;
+};
+
 function Tabs() {
   return (
-    <Tab.Navigator initialRouteName="Home" screenOptions={{headerShown: false}}>
+    <Tab.Navigator
+      initialRouteName="Home"
+      screenOptions={({route}) => {
+        return {
+          tabBarIcon: ({color}) => screenOptions(route.name, color),
+          tabBarActiveTintColor: 'white',
+          tabBarInactiveTintColor: 'white',
+          tabBarActiveBackgroundColor: Color(theme.theme.colors.primary)
+            .alpha(0.8)
+            .rgb()
+            .string(),
+          tabBarInactiveBackgroundColor: theme.theme.colors.primary,
+          headerShown: false,
+        };
+      }}>
       <Tab.Screen name="Muestras" component={Muestras} />
       <Tab.Screen name="Home" component={Home} />
       <Tab.Screen name="Vademecum" component={Vademecum} />
