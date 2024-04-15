b53c73761535b950811190b5f707f7a0a1898f00
maitzeth <andre801mz@gmail.com>
2024-04-15 01:12:55 -0300

    feat: adding icons

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 import {Button} from 'react-native';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 export function Home({navigation}: any) {
   return (
     <View>
       <Text>Home</Text>
+      <AntDesign name="customerservice" style={{
+        color: 'red',
+        fontSize: 50,
+      }} />
+      <AntDesign name="windows" style={{
+        color: 'red',
+        fontSize: 50,
+      }} />
       <Button
         title="Go Details"
         onPress={() => {
